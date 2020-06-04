fb88b0804411a53a1be88e27b5526996daec9210
Ashar77 <Asharmehmood360@gmail.com>
2020-06-04 15:08:44 -0400

    first

---
+++ build/precache-manifest.31586d7f593c5c129665370f7a83faca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06418a82dc0fa291ff33ccaa80b5829b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daec0e5544825e38cde1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a204ff552143c2e7602",
+    "url": "/static/js/2.69b3b360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69b3b360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daec0e5544825e38cde1",
+    "url": "/static/js/main.c2ded30c.chunk.js"
+  },
+  {
+    "revision": "aa10e2155fefa269aa48",
+    "url": "/static/js/runtime-main.a9a2cc50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
