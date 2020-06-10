b2c6ed548594077840097df2cc6934db5a0fc1df
Ashar77 <Asharmehmood360@gmail.com>
2020-06-10 16:23:59 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the js react app .
+          Edit <code>src/App.js</code> and save to do not  reload the js react app .
         </p>
 
        
